6ff98fff7860df6d0c93231f4beb645e4b88569d
Noah Fajarda <noahfajarda@gmail.com>
2023-08-03 14:12:00 -0700

    added base routes for login/signup

---
+++ server/routers/authRouter.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const validateForm = require("../controllers/validateForm")
+
+
+
+router.post("/login", (req, res) => {
+  validateForm(req, res)
+})
+
+router.post("/signup", (req, res) => {
+  validateForm(req, res)
+})
+
+module.exports = router;
