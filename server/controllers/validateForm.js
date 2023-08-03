8624e3727ba239ae610f69a9d548ebfeb093215e
Noah Fajarda <noahfajarda@gmail.com>
2023-08-03 14:12:45 -0700

    added signup/login formvalidation

---
+++ server/controllers/validateForm.js
@@ -0,0 +1,28 @@
+const Yup = require("yup")
+
+const formSchema = Yup.object({
+  username: Yup.string()
+    .required("Username required")
+    .min(6, "Username too short")
+    .max(28, "Username too long"),
+  password: Yup.string()
+    .required("Password required")
+    .min(6, "Password too short")
+    .max(28, "Password too long"),
+})
+
+
+const validateForm = (req, res) => {
+  const formData = req.body;
+  // server-side form validation
+  formSchema.validate(formData).catch(err => {
+    console.log(err.errors);
+    res.status(422).send();
+  }).then(valid => {
+    if (valid) {
+      console.log("Form is good")
+    }
+  })
+}
+
+module.exports = validateForm
