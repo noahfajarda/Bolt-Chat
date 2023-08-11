b5b70ce572854d6799a90382d7a357b0e37fcbfc
Noah Fajarda <noahfajarda@gmail.com>
2023-08-10 23:08:46 -0700

    compartmentalized context

--- client/src/components/AccountContext.jsx
+++ client/src/components/AccountContext.jsx
@@ -1,10 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { getLoggedInUserData } from "../utils/APIcalls";
 
 export const AccountContext = createContext();
 
 export const UserContext = ({ children }) => {
   const [user, setUser] = useState({ loggedIn: null });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getLoggedInUserData(setUser, navigate);
+  }, []);
+
   return (
+    // allow user to be manipulated and accessed by other components
     <AccountContext.Provider value={{ user, setUser }}>
       {children}
     </AccountContext.Provider>
