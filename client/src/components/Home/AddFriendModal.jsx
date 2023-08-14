f690c85cd603ecce4f32866fa6fdb04dbd5e49ca
Noah Fajarda <noahfajarda@gmail.com>
2023-08-14 14:16:22 -0700

    added modal to search for friend

---
+++ client/src/components/Home/AddFriendModal.jsx
@@ -0,0 +1,55 @@
+import { Modal } from "@chakra-ui/modal";
+import {
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+import TextField from "../TextField";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+
+export default function AddFriendModal({ isOpen, onClose }) {
+  const friendSchema = Yup.object({
+    friendName: Yup.string()
+      .required("Username required")
+      .min(6, "Invalid username!")
+      .max(28, "Invalid username!"),
+  });
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add a friend!</ModalHeader>
+        <ModalCloseButton />
+        <Formik
+          initialValues={{ friendName: "" }}
+          onSubmit={(values) => {
+            onClose();
+          }}
+          validationSchema={friendSchema}
+        >
+          <Form>
+            <ModalBody>
+              <TextField
+                label="friend's name"
+                placeholder="Enter friend's username..."
+                autocomplete="off"
+                name="friendName"
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" type="submit">
+                Submit
+              </Button>
+            </ModalFooter>
+          </Form>
+        </Formik>
+      </ModalContent>
+    </Modal>
+  );
+}
--- client/src/components/Home/Sidebar.jsx
+++ client/src/components/Home/Sidebar.jsx
@@ -7,35 +7,41 @@ import {
   Heading,
   VStack,
   Circle,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { Tab, TabList } from "@chakra-ui/tabs";
 import { useContext } from "react";
 import { FriendContext } from "./Home";
+import AddFriendModal from "./AddFriendModal";
 
 export default function Sidebar() {
   const { friendList, setFriendList } = useContext(FriendContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <VStack py="1.4rem">
-      <HStack justify="space-evenly" w="100%">
-        <Heading size="md">Add Friend</Heading>
-        <Button>
-          <ChatIcon />
-        </Button>
-      </HStack>
-      <Divider />
-      <VStack as={TabList}>
-        {friendList.map((friend) => (
-          <HStack as={Tab} key={friend?.username}>
-            <Circle
-              bg={friend.connected ? "green.700" : "red.500"}
-              w="20px"
-              h="20px"
-            />
-            <Text>{friend.username}</Text>
-          </HStack>
-        ))}
+    <>
+      <VStack py="1.4rem">
+        <HStack justify="space-evenly" w="100%">
+          <Heading size="md">Add Friend</Heading>
+          <Button onClick={onOpen}>
+            <ChatIcon />
+          </Button>
+        </HStack>
+        <Divider />
+        <VStack as={TabList}>
+          {friendList.map((friend) => (
+            <HStack as={Tab} key={friend?.username}>
+              <Circle
+                bg={friend.connected ? "green.700" : "red.500"}
+                w="20px"
+                h="20px"
+              />
+              <Text>{friend.username}</Text>
+            </HStack>
+          ))}
+        </VStack>
       </VStack>
-    </VStack>
+      <AddFriendModal isOpen={isOpen} onClose={onClose} />
+    </>
   );
 }
