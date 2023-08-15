3188b3209a2c2927460f02cf0c165fea4f78bd2d
Noah Fajarda <noahfajarda@gmail.com>
2023-08-15 11:50:00 -0700

    fixed minor console errors

--- client/src/theme.js
+++ client/src/theme.js
@@ -9,12 +9,12 @@ const theme = {
     global: {
       body: {
         margin: 0,
-        "font-family": "-apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', Helvetica Neue', sans-serif",
-        "-webkit-font-smoothing": "antialiased",
-        "-moz-osx-font-smoothing": "grayscale",
+        fontFamily: "-apple-system, BlinkMacSystemFont,'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', Helvetica Neue', sans-serif",
+        WebkitFontSmoothing: "antialiased",
+        MozOsxFontSmoothing: "grayscale",
       },
       code: {
-        "font-family": "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace",
+        fontFamily: "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace",
       }
     }
   }
