8ff025c038746d036cd8a687397748b99dcc3c60
Noah Fajarda <noahfajarda@gmail.com>
2023-08-15 14:02:33 -0700

    added socket.connect to attempt to connect to socket.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import ToggleColorMode from "./components/ToggleColorMode";
 import Views from "./components/Views";
 import { UserContext } from "./components/AccountContext";
+import socket from "./socket";
 
 function App() {
+  // attempt to connect to socket
+  socket.connect();
   return (
     <UserContext>
       <Views />
