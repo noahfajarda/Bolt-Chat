2693f5811bb20e33671ddf59adc1494d9e998bb6
Noah Fajarda <noahfajarda@gmail.com>
2023-08-26 22:52:12 -0700

    added proxy config for vite for routes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const routeConfig = {
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+  secure: false,
+  ws: true
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth/signup': routeConfig,
+      '/auth/login': routeConfig,
+    }
+  }
 })
