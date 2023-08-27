5dae09805b3b422d49f7d55ee7a7b8f7cce30e2d
Noah Fajarda <noahfajarda@gmail.com>
2023-08-26 22:57:12 -0700

    added functions for sockets to add friends and initialize users

--- server/controllers/socketController.js
+++ server/controllers/socketController.js
@@ -14,3 +14,48 @@ module.exports.authorizeUser = (socket, next) => {
     next()
   }
 }
+
+module.exports.initializeUser = async (socket) => {
+  // retrieve socket for user
+  console.log(socket.request.session)
+  socket.user = { ...socket.request.session.user };
+
+  // set the socket in redis for the current user
+  await redisClient.hset(
+    `userid:${socket.user.username}`,
+    "userid",
+    socket.user.userid
+  )
+
+  // retrieve & set the friends of the current user with redis
+  const friendList = await redisClient.lrange(`friends:${socket.user.username}`, 0, -1)
+  socket.emit("friends", friendList)
+}
+
+module.exports.addFriend = async (socket, friendName, cb) => {
+  // check if attempted friend == logged in user
+  if (friendName === socket.user.username) {
+    cb({ done: false, errorMsg: "Cannot add self!" })
+    return
+  }
+
+  // check if user exists
+  const friendUserId = await redisClient.hget(`userid:${friendName}`, "userid")
+  // check if user is already in friend's list
+  const currentFriendsList = await redisClient.lrange(`friends:${socket.user.username}`, 0, -1)
+
+  // if no user exists, show error and don't add as friend
+  if (!friendUserId) {
+    cb({ done: false, errorMsg: "User doesn't exist!" })
+    return;
+  }
+
+  // if the friend is already in the list, show error and don't add as friend
+  if (currentFriendsList && currentFriendsList.indexOf(friendName) !== -1) {
+    cb({ done: false, errorMsg: "Friend already added!" })
+  }
+
+  // add the friend to redis
+  await redisClient.lpush(`friends:${socket.user.username}`, friendName);
+  cb({ done: true })
+}
