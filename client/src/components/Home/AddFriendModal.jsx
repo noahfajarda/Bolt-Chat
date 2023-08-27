9eb58a4d17d0f7531a50d7443917fd6a18960a73
Noah Fajarda <noahfajarda@gmail.com>
2023-08-26 22:38:41 -0700

    able to add friend with data persistance

--- client/src/components/Home/AddFriendModal.jsx
+++ client/src/components/Home/AddFriendModal.jsx
@@ -7,12 +7,19 @@ import {
   ModalFooter,
   ModalCloseButton,
   Button,
+  Heading,
 } from "@chakra-ui/react";
 import TextField from "../TextField";
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
+import socket from "../../socket";
+import { useCallback, useContext, useState } from "react";
+import { FriendContext } from "./Home";
 
 export default function AddFriendModal({ isOpen, onClose }) {
+  const { setFriendList } = useContext(FriendContext);
+  const [error, setError] = useState("");
+
   const friendSchema = Yup.object({
     friendName: Yup.string()
       .required("Username required")
@@ -20,8 +27,27 @@ export default function AddFriendModal({ isOpen, onClose }) {
       .max(28, "Invalid username!"),
   });
 
+  // close modal function
+  const closeModal = useCallback(() => {
+    setError("");
+    onClose();
+  }, [onClose]);
+
+  // function to trigger event to server & add values
+  const attemptToAddFriend = (values) => {
+    socket.emit("add_friend", values.friendName, ({ errorMsg, done }) => {
+      if (done) {
+        setFriendList((prev) => [values.friendName, ...prev]);
+        closeModal();
+        return;
+      }
+      // show an error
+      setError(errorMsg);
+    });
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+    <Modal isOpen={isOpen} onClose={closeModal} isCentered>
       {/* darkens background */}
       <ModalOverlay />
       <ModalContent>
@@ -29,15 +55,16 @@ export default function AddFriendModal({ isOpen, onClose }) {
         <ModalCloseButton />
         <Formik
           initialValues={{ friendName: "" }}
-          onSubmit={(values) => {
-            onClose();
-          }}
+          onSubmit={(values) => attemptToAddFriend(values)}
           validationSchema={friendSchema}
         >
           <Form>
             <ModalBody>
+              <Heading fontSize="xl" as="p" color="red.500" textAlign="center">
+                {error}
+              </Heading>
               <TextField
-                label="friend's name"
+                label="Friend's name"
                 placeholder="Enter friend's username..."
                 autoComplete="off"
                 name="friendName"
