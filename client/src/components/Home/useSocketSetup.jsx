e8d15388b7e92b710f8980782a30ec8896b49cf7
Noah Fajarda <noahfajarda@gmail.com>
2023-08-26 22:49:47 -0700

    updated custom socket hook

--- client/src/components/Home/useSocketSetup.jsx
+++ client/src/components/Home/useSocketSetup.jsx
@@ -2,14 +2,15 @@ import { useContext, useEffect } from "react";
 import socket from "../../socket";
 import { AccountContext } from "../AccountContext";
 
-const useSocketSetup = () => {
+const useSocketSetup = (setFriendList) => {
   const { setUser } = useContext(AccountContext);
   useEffect(() => {
     // attempt to connect to socket server
     socket.connect();
+    socket.on("friends", (friendList) => setFriendList(friendList));
     // if socket connection fails, set user log in to false
     socket.on("connect_error", () => {
-      setUser({ loggedIn: false });
+      // setUser({ loggedIn: false });
     });
     return () => {
       socket.off("connect_error");
