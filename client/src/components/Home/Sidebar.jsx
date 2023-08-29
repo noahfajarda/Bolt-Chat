5a137b65784e7d9a8c6e74132f38b785c92421c3
Noah Fajarda <noahfajarda@gmail.com>
2023-08-28 21:00:17 -0700

    added log out button

--- client/src/components/Home/Sidebar.jsx
+++ client/src/components/Home/Sidebar.jsx
@@ -19,7 +19,14 @@ export default function Sidebar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+      }}
+    >
       <VStack py="1.4rem">
         <HStack justify="space-evenly" w="100%">
           <Heading size="md">Add Friend</Heading>
@@ -29,19 +36,47 @@ export default function Sidebar() {
         </HStack>
         <Divider />
         <VStack as={TabList} style={{ borderBottom: "none" }}>
-          {friendList.map((friend, idx) => (
-            <HStack as={Tab} key={idx}>
-              <Circle
-                bg={friend?.connected ? "green.700" : "red.500"}
-                w="20px"
-                h="20px"
-              />
-              <Text>{friend?.username}</Text>
-            </HStack>
-          ))}
+          {friendList.map((friend, idx) => {
+            return (
+              <HStack as={Tab} key={idx}>
+                <Circle
+                  bg={friend?.connected ? "green.700" : "red.500"}
+                  w="20px"
+                  h="20px"
+                />
+                <Text>{friend?.username}</Text>
+              </HStack>
+            );
+          })}
         </VStack>
       </VStack>
+      {/* log out button */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Button
+          colorScheme="teal"
+          style={{
+            position: "relative",
+            top: "0px",
+            margin: 15,
+            marginBottom: 20,
+            padding: "1.4rem",
+          }}
+          onClick={async () => {
+            sessionStorage.removeItem("user");
+            location.reload();
+            window.close();
+          }}
+        >
+          Log Out
+        </Button>
+      </div>
+
       <AddFriendModal isOpen={isOpen} onClose={onClose} />
-    </>
+    </div>
   );
 }
